f02ab04e86b680707a8aec0d543278c454024332
Alexis <172384082+alexissyi@users.noreply.github.com>
2025-11-22 18:00:11 -0500

    Add more Group tests

--- src/concepts/Group/GroupConcept.test.ts
+++ src/concepts/Group/GroupConcept.test.ts
@@ -53,6 +53,20 @@ Deno.test(
         "Incorrect group request"
       );
 
+      const groupRequests = await groupConcept._getGroupRequests({
+        group: groupObject.group,
+      });
+
+      assertEquals(groupRequests.length, 1);
+
+      const groupRequest = groupRequests[0];
+      assertEquals(groupRequest.requester, user, "Incorrect user");
+      assertEquals(
+        groupRequest.membershipRequest,
+        membershipRequest.membershipRequest,
+        "Incorrect group request"
+      );
+
       console.log("Successfully created a membership request");
 
       await groupConcept.accept({
@@ -75,3 +89,95 @@ Deno.test(
     }
   }
 );
+
+Deno.test("Action: deny", async () => {
+  console.log("\n🧪 TEST CASE 2: deny");
+  console.log("==================================");
+  const [db, client] = await testDb();
+  try {
+    const groupConcept = new GroupConcept(db);
+    const name = "Gym";
+    const leader = freshID() as User;
+    const privateGroup = false;
+    const groupObject = (await groupConcept.create({
+      name: name,
+      leader: leader,
+      privateGroup: privateGroup,
+    })) as { group: Group };
+
+    const publicGroups = await groupConcept._getPublicGroups({});
+
+    const publicGroup = publicGroups[0];
+
+    const user = freshID() as User;
+
+    const membershipRequest = (await groupConcept.request({
+      user: user,
+      group: groupObject.group,
+    })) as { membershipRequest: MembershipRequest };
+
+    const userRequests = await groupConcept._getUserRequests({ user: user });
+
+    const userRequest = userRequests[0];
+
+    await groupConcept.deny({
+      membershipRequest: membershipRequest.membershipRequest,
+    });
+
+    const groupRequests = await groupConcept._getGroupRequests({
+      group: groupObject.group,
+    });
+
+    assertEquals(groupRequests.length, 0);
+
+    const members = await groupConcept._getMembers({
+      group: groupObject.group,
+    });
+
+    assertEquals(members.length, 0);
+  } finally {
+    await client.close();
+  }
+});
+
+Deno.test("Action: leave", async () => {
+  console.log("\n🧪 TEST CASE 3: leave");
+  console.log("==================================");
+  const [db, client] = await testDb();
+  try {
+    const groupConcept = new GroupConcept(db);
+    const name = "Gym";
+    const leader = freshID() as User;
+    const privateGroup = false;
+    const groupObject = (await groupConcept.create({
+      name: name,
+      leader: leader,
+      privateGroup: privateGroup,
+    })) as { group: Group };
+
+    const publicGroups = await groupConcept._getPublicGroups({});
+
+    const publicGroup = publicGroups[0];
+
+    const user = freshID() as User;
+
+    const membershipRequest = (await groupConcept.request({
+      user: user,
+      group: groupObject.group,
+    })) as { membershipRequest: MembershipRequest };
+
+    await groupConcept.accept({
+      membershipRequest: membershipRequest.membershipRequest,
+    });
+
+    await groupConcept.leave({ user: user, group: groupObject.group });
+
+    const members = await groupConcept._getMembers({
+      group: groupObject.group,
+    });
+
+    assertEquals(members.length, 0);
+  } finally {
+    await client.close();
+  }
+});
