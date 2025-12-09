0d96307594ffc3c3f7d9abf4609a8a38893b8098
Luis <luis0818@mit.edu>
2025-12-09 10:50:34 -0500

    Added sync logic for UserProfile

---
+++ src/syncs/userProfile.sync.ts
@@ -0,0 +1,40 @@
+import { Requesting, UserProfile } from "@concepts";
+import { actions, Sync } from "@engine";
+
+// User Profile creation handled in UserAuthentication syncs
+
+// User Profile editing request
+
+export const UserProfileEditRequest: Sync = ({ request, user, location, bio, skillLevel, userImg }) => ({
+    when: actions([
+        Requesting.request,
+        { path : "/editProfile", user, location, bio, skillLevel, userImg },
+        { request },
+    ]),
+    then: actions([
+        UserProfile.editProfile,
+        { user, location, bio, skillLevel, userImg },
+    ]),
+});
+
+export const UserProfileEditResponse: Sync = ({ request, user}) => ({
+    when: actions(
+        [Requesting.request, { path : "/editProfile", user }, { request }],
+        [UserProfile.editProfile, { user }, {}],
+    ),
+    then: actions([
+        Requesting.respond, { user, request, status: "profile updated" },
+    ]),
+});
+
+export const UserProfileEditError: Sync = ({ request, user, error }) => ({
+    when: actions(
+        [Requesting.request, { path : "/editProfile" }, { request }],
+        [UserProfile.editProfile, { user }, { error}],
+    ),
+    then: actions([
+        Requesting.respond, { request, error, user},
+    ]),
+});
+
+//getUserProfile request public query
